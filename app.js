61fccd1864f3ff1111f70267fd8fde25297d48ea
Jeremy Dowell <jeremy.dowell@architectinginnovation.com>
2013-07-19 01:59:03 -0500

    Names and tweaks

--- app.js
+++ app.js
@@ -10,11 +10,19 @@ var app = require('express.io')()
   , http = require('http')
   , path = require('path')
   , twitter = require('ntwitter')
+  , dirty = require('dirty')
+  , EventEmitter = require('events').EventEmitter
+  , emitter = new EventEmitter()
+  , totalTweets = 0
+  , totalErrors = 0;
+
+
+
 
 app.http().io();
 
 // all environments
-app.set('port', process.env.PORT || 1127);
+app.set('port', 1127);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -32,18 +40,24 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+emitter.on('new city', getCityInfo);
+
 var twit = new twitter({
-	consumer_key: 'oL9WYc0haBx8S8VmHBCTQ',
-	consumer_secret: '2Pfxk3sl9rhtbEtn4TUlUiR9NDlu7x0puuB4rihXbQ',
-	access_token_key: '845732328-HqzZS5itaQmsppAoR1VM14yhhIg7WsRepkhlBJTp',
-	access_token_secret: 'fPW3Tu0WCPSPK6DKZGqzD4reN1amq8XMydjPj5wbTs'
+	consumer_key: 'ftf8DUtrpa3TOSPjZBOIEQ',
+	consumer_secret: 'j9w4DhzvQ0dWFxQr5TqXh9NrhJcpX8UZ1bvGW1LY',
+	access_token_key: '845732328-14o53Xqi95T7wZ4EUDt9Lc7eNa8qBL7zMuzHskVg',
+	access_token_secret: 'OWNOwB7A2MzAmImhmxx5q1AWQPqWp3IsqLTqIZchdk'
 });
 
 
 twit.stream('statuses/filter', {locations: '-124.848974,24.396308,-66.885444,49.384358' }, function(stream) {
 	stream.on('data',function(data) {
+		totalTweets++;
 		try {
-			app.io.broadcast('tweet',data.coordinates.coordinates);
+			var dataObj = {
+				coords: data.coordinates.coordinates
+			}
+			app.io.broadcast('tweet',dataObj);
 		} catch(e) {
 			
 		}
@@ -57,5 +71,11 @@ twit.stream('statuses/filter', {locations: '-124.848974,24.396308,-66.885444,49.
 	})
 });
 
+function getCityInfo() {
 
-app.listen(3000);
+}
+
+var db = dirty('locations.db')
+
+
+app.listen(app.get('port'));
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,44 +1,179 @@
-socket = io.connect()
-//socket = {};
+$(function() {
+		socket = io.connect()
+	//socket = {};
 
-var height = $(document).height();
-var width = $(document).width();
-try{
-	var canvas = document.getElementsByTagName('canvas')[0];
-    var heatmap = createWebGLHeatmap({height:height, width: width});
-    document.body.appendChild(heatmap.canvas);
+	var height = $(document).height();
+	var width = $(document).width();
+	var cities = [
+		{
+			name: 'Dallas',
+			location: {
+				lat: 32.780140,
+				lon: -96.800451
+			}
+		}, 
+		{
+			name: 'San Diego',
+			location: {
+				lat: 32.715329,
+				lon: -117.157255
+			}
+		},
+		{
+			name: 'Los Angeles',
+			location: {
+				lat: 34.052234,
+				lon: -118.243685
+			}
+		},
+		{
+			name: 'San Francisco',
+			location: {
+				lat: 37.774929,
+				lon: -122.419416
+			}
+		},
+		{
+			name: 'New York',
+			location: {
+				lat: 40.714353,
+				lon: -74.005973
+			}
+		},
+		{
+			name: 'Boston',
+			location: {
+				lat: 42.358431,
+				lon: -71.059773
+			}
+		},
+		{
+			name: 'Chicago',
+			location: {
+				lat: 41.878114,
+				lon: -87.629798
+			}
+		},
+		{
+			name: 'Miami',
+			location: {
+				lat: 25.788969,
+				lon: -80.226439
+			}
+		},
+		{
+			name: 'Tampa',
+			location: {
+				lat: 27.950575,
+				lon: -82.457178
+			}
+		},
+		{
+			name: 'Houston',
+			location: {
+				lat: 29.760193,
+				lon: -95.369390
+			}
+		},
+		{
+			name: 'Austin',
+			location: {
+				lat: 30.267153,
+				lon: -97.743061
+			}
+		},
+		{
+			name: 'Seattle',
+			location: {
+				lat: 47.606209,
+				lon: -122.332071
+			}
+		},
+		{
+			name: 'Seattle',
+			location: {
+				lat: 47.606209,
+				lon: -122.332071
+			}
+		},
+		{
+			name: 'Portland',
+			location: {
+				lat: 45.523452,
+				lon: -122.676207
+			}
+		},
+		{
+			name: 'D.C.',
+			location: {
+				lat: 38.907231,
+				lon: -77.036464
+			}
+		},
+		{
+			name: 'Bakersfield',
+			location: {
+				lat: 35.373292,
+				lon: -119.018712
+			}
+		},
 
-}
-catch(error){
-    console.log(error);
-}
-
-var topX = 0;
-// Listen for the new visitor event.
-socket.on('tweet', function(data) {
-	var y = Math.abs(((Math.ceil(data[1] - 25) / 25) * height)-height);
-	var x = (Math.ceil(data[0] + 124)/58) * width ;
-	//total width: 58
-	//total height: 25
-    heatmap.addPoint(x,y,30,.25);
-})
-
-$('#disconnect').click(function() {
-	socket.disconnect();
-})
 
 
- var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                                         window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+	];
 
-var update = function(){
-    //heatmap.addPoint(100, 100, 100, 10/255);
-    heatmap.adjustSize(); // can be commented out for statically sized heatmaps, resize clears the map
-    heatmap.update(); // adds the buffered points
-    heatmap.display(); // adds the buffered points
-    heatmap.multiply(0.9999);
-    heatmap.blur();
-    //heatmap.clamp(0.0, 1.0); // depending on usecase you might want to clamp it
-    raf(update);
-}
-raf(update);
+
+
+	try{
+		var canvas = document.getElementsByTagName('canvas')[0];
+	    var heatmap = createWebGLHeatmap({height:height, width: width});
+	    document.body.appendChild(heatmap.canvas);
+	}
+	catch(error){
+	    console.log(error);
+	}
+
+	
+	var c = document.getElementById("labels");
+	var ctx = c.getContext("2d");
+	ctx.canvas.width = $(window).width();
+	ctx.canvas.height = $(window).height();
+	ctx.font="10px Roboto";
+	ctx.fillStyle = '#222222';
+
+	_.each(cities, function(city) {
+		var textSize = ctx.measureText(city.name);
+		var x = ((Math.ceil(city.location.lon + 124.848974)/58) * width) - (width * .025) ;
+		var y = Math.abs(((Math.ceil(city.location.lat - 24.396308) / 25) * height)-height) + (width * .025);
+		ctx.fillText(city.name,x,y);
+		
+	})
+
+
+	socket.on('tweet', function(data) {
+		
+		var y = Math.abs(((Math.ceil(data.coords[1] - 25) / 25) * height)-height);
+		var x = (Math.ceil(data.coords[0] + 124)/58) * width ;
+	    heatmap.addPoint(x,y,30,.25);
+	})
+
+	$('#disconnect').click(function() {
+		socket.disconnect();
+	})
+
+
+	 var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+	                                         window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+	var update = function(){
+	    //heatmap.addPoint(100, 100, 100, 10/255);
+	    heatmap.adjustSize(); // can be commented out for statically sized heatmaps, resize clears the map
+	    heatmap.update(); // adds the buffered points
+	    heatmap.display(); // adds the buffered points
+	    heatmap.multiply(0.9999);
+	    heatmap.blur();
+	    heatmap.clamp(0.0, .9); // depending on usecase you might want to clamp it
+	    raf(update);
+	}
+	raf(update);
+});
